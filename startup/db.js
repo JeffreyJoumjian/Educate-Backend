c44b7e932098359f27dcdf2501ddecbc21650c4c
skylerdj <jeffreyjoumjian@hotmail.com>
2021-05-25 20:47:07 +0300

    connecting to online db

--- startup/db.js
+++ startup/db.js
@@ -2,10 +2,12 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 
 module.exports = function () {
-	const db = "mongodb://127.0.0.1:27017/Educate";
 
-	// const db = `${config.get('db.host')}&authSource=${config.get('db.auth')}&w=${config.get('db.w')}`
-	// const db = `${process.env.DB_HOST}&authSource=${process.env.DB_AUTH}&w=${process.env.DB_W}`
+	const { DEV_MODE, DB_USER, DB_PASSWORD, DB_HOST, DB_AUTH, DB_W } = process.env;
+
+	const db = DEV_MODE === "online" ?
+		`mongodb+srv://${DB_USER}:${DB_PASSWORD}${DB_HOST}?${DB_AUTH}&${DB_W}`
+		: "mongodb://127.0.0.1:27017/Educate";
 
 	mongoose.connect((db), {
 		useCreateIndex: true,
