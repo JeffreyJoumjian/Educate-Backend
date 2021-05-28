038d4538c6283c245951360131dd40ff5c2ac501
skylerdj <jeffreyjoumjian@hotmail.com>
2021-05-28 04:41:27 +0300

    send expires in

--- routes/logins.js
+++ routes/logins.js
@@ -25,7 +25,7 @@ function generateLoginToken(user) {
 		process.env.EDUCATE_PRIVATE_KEY,
 		{ expiresIn: '1h' }
 	);
-	return { token, userType, expiresIn: 1 };
+	return { token, userType, expiresIn: 3600 };
 }
 
 router.post('/', async (req, res) => {
